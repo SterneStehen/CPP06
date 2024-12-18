8845dd274b71f50259ad6bc65cb8267538f19705
Sternestehen <7353718@google.com>
2024-12-18 23:03:03 +0100

    test git

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,30 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoreron <7353718@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 23:02:20 by smoreron          #+#    #+#             */
+/*   Updated: 2024/12/18 23:02:40 by smoreron         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+include <iostream>
+
+class ScalarConverter
+{
+private:
+	/* data */
+public:
+	ScalarConverter(/* args */);
+	~ScalarConverter();
+};
+
+ScalarConverter::ScalarConverter(/* args */)
+{
+}
+
+ScalarConverter::~ScalarConverter()
+{
+}
