2f3c2d989eb753cf3525d43babf628e7e59b0ee4
Sternestehen <7353718@google.com>
2025-01-09 09:02:53 +0100

    add exercise ex02

---
+++ ex02/Base.cpp
@@ -0,0 +1,64 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoreron <7353718@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 07:36:17 by smoreron          #+#    #+#             */
+/*   Updated: 2025/01/09 09:02:01 by smoreron         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#include "Base.hpp"
+
+Base * generate(void){
+	std::srand(time(NULL));
+	int random = rand() % 3;
+	if(random == 0){
+		std::cout << "return A" << std::endl;
+		return new A;
+	}
+	else if (random == 1){
+		std::cout << "return B" << std::endl;
+		return new B;
+	}
+	else
+	{
+		std::cout << "return C" << std::endl;
+		return new C;
+	}
+}
+void identify(Base* p){
+	if(dynamic_cast<A*>(p))
+		std::cout << "result identify * is " << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "result identify * is " << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "result identify * is " << "C" << std::endl;
+	else
+		std::cout << "result identify * is " << "unknown type "<< std::endl;
+}
+
+void identify(Base& p){
+	try{
+		A &a = dynamic_cast<A&>(p);
+		std::cout << "result identify & is " << "A" << std::endl;
+		return;
+	}
+	catch(std::bad_cast &bc){}
+	try{
+		B &b = dynamic_cast<B&>(p);
+		std::cout << "result identify & is " << "B" << std::endl;
+		return;
+	}
+	catch(...){}
+	try{
+		C &c = dynamic_cast<C&>(p);
+		std::cout << "result identify & is " << "C" << std::endl;
+		return;
+	}
+	catch(...){}
+	std::cout << "result identify & is " << "Unknown type" << std::endl;	
+}
+
---
+++ ex02/Base.hpp
@@ -0,0 +1,33 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smoreron <7353718@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 08:47:22 by smoreron          #+#    #+#             */
+/*   Updated: 2025/01/09 09:02:28 by smoreron         ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#ifndef BASE_HPP
+#define BASE_HPP
+
+
+#include <cstdlib>
+#include <ctime> 
+#include <iostream>
+
+class Base{
+public:
+	virtual ~Base(){};
+};
+class A: public Base{};
+class B: public Base{};
+class C: public Base{};
+
+Base * generate(void); 
+void identify(Base* p);
+void identify(Base& p);
+
+#endif
